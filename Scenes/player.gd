c4c9b705d77abc688b1ab5f1576dae90c0e848e7
Andrew Ashby <andrewashby101@gmail.com>
2024-09-09 22:49:40 -0700

    Added Free Assets

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -14,9 +14,27 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
-			#camera.rotation += Vector3(-event.relative.y*0.01, -event.relative.x*0.01, 0)
 			camera.rotation.x = clamp(camera.rotation.x - event.relative.y*camera_sensitivity, -PI/2, PI/2)
 			rotation.y += -event.relative.x*camera_sensitivity
+	
+	
+## Drag and drop
+#func _input(event: InputEvent) -> void:
+	#var intersect = get_mouse_intersect(event.position)
+	#
+	#if event is InputEventMouse:
+		#if intersect: mouse_position = intersect.position
+	#
+	#if event is InputEventMouseButton:
+		#var left_button_pressed = event.button_index == MOUSE_BUTTON_LEFT and event.pressed
+		#var left_button_released = event.button_index == MOUSE_BUTTON_LEFT and not event.pressed
+		#
+		#if left_button_released:
+			#dragging = false
+			#drag_and_drop(intersect)
+		#elif left_button_pressed:
+			#dragging = true
+			#drag_and_drop(intersect)
 
 
 func _physics_process(delta: float) -> void:
@@ -40,3 +58,32 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, movement_speed)
 
 	move_and_slide()
+
+
+
+# Drag and drop code:
+var dragging_collider
+var mouse_position
+var dragging := false
+
+func _process(delta: float) -> void:
+	if dragging_collider:
+		dragging_collider.global_position = mouse_position
+
+func drag_and_drop(intersect : Dictionary) -> void:
+	if not dragging_collider and dragging:
+		dragging_collider = intersect.collider
+		dragging_collider.set_collision_layer(false)
+	elif dragging_collider:
+		dragging_collider.set_collision_layer(true)
+		dragging_collider = null
+		
+
+func get_mouse_intersect(mouse_pos) -> Dictionary:
+	var current_camera := get_viewport().get_camera_3d()
+	var params := PhysicsRayQueryParameters3D.new()
+	params.from = current_camera.project_ray_origin(mouse_pos)
+	params.to = current_camera.project_position(mouse_pos, 1000)
+	
+	var world_space := get_world_3d().direct_space_state
+	return world_space.intersect_ray(params)
